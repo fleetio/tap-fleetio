4c8b35f4e047b1eb65d95fdd43b0afcfec1fc498
John Mizerany <jmizerany@fleetio.com>
2023-09-06 16:26:22 -0500

    add in replcation keys

--- tap_fleetio/streams.py
+++ tap_fleetio/streams.py
@@ -14,7 +14,7 @@ class IssuesStream(fleetioStream):
     name = "issues"
     path = "/v2/issues"
     primary_keys = ["id"]
-    replication_key = None
+    replication_key = "updated_at"
     schema_filepath = SCHEMAS_DIR / "issues.json"  # noqa: ERA001
 
 
@@ -23,7 +23,7 @@ class ServiceEntriesStream(fleetioStream):
     name = "service_entries"
     path = "/v2/service_entries"
     primary_keys = ["id"]
-    replication_key = None
+    replication_key = "updated_at"
     schema_filepath = SCHEMAS_DIR / "service_entries.json"  # noqa: ERA001
 
 
@@ -32,7 +32,7 @@ class SubmittedInspectionFormsStream(fleetioStream):
     name = "submitted_inspection_forms"
     path = "/v1/submitted_inspection_forms"
     primary_keys = ["id"]
-    replication_key = None
+    replication_key = "submitted_at"
     schema_filepath = SCHEMAS_DIR / "submitted_inspection_forms.json"  # noqa: ERA001
 
 
@@ -41,5 +41,5 @@ class VehiclesStream(fleetioStream):
     name = "vehicles"
     path = "/v1/vehicles"
     primary_keys = ["id"]
-    replication_key = None
+    replication_key = "updated_at"
     schema_filepath = SCHEMAS_DIR / "vehicles.json"  # noqa: ERA001
