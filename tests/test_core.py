133d335015e0b989f9e61b87638510e0f802133b
John Mizerany <jmizerany@fleetio.com>
2023-11-30 09:42:05 -0600

    add env variables

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,6 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
+import os
 import datetime
 
 from singer_sdk.testing import get_tap_test_class
@@ -8,8 +9,8 @@ from tap_fleetio.tap import Tapfleetio
 
 SAMPLE_CONFIG = {
     "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    "api_token": "xxx",
-    "account_token": "xxx"
+    "api_token": os.environ.get("TAP_FLEETIO_API_TOKEN"),
+    "account_token": os.environ.get("TAP_FLEETIO_ACCOUNT_TOKEN")
 }
 
 
