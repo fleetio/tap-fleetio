36b2caa95174d0df2dbe8c5aa8a5a0478b48045a
John Mizerany <jmizerany@fleetio.com>
2024-01-17 10:09:26 -0600

    add configs to switch between API versions, remove replication keys

--- tap_fleetio/streams.py
+++ tap_fleetio/streams.py
@@ -14,7 +14,7 @@ class IssuesStream(fleetioStream):
     name = "issues"
     path = "/v2/issues"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2023-03-01"
     schema_filepath = SCHEMAS_DIR / "issues.json"  # noqa: ERA001
 
 
@@ -23,7 +23,7 @@ class ServiceEntriesStream(fleetioStream):
     name = "service_entries"
     path = "/v2/service_entries"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2023-03-01"
     schema_filepath = SCHEMAS_DIR / "service_entries.json"  # noqa: ERA001
 
 
@@ -32,7 +32,7 @@ class SubmittedInspectionFormsStream(fleetioStream):
     name = "submitted_inspection_forms"
     path = "/v1/submitted_inspection_forms"
     primary_keys = ["id"]
-    replication_key = "submitted_at"
+    api_version = "2023-03-01"
     schema_filepath = SCHEMAS_DIR / "submitted_inspection_forms.json"  # noqa: ERA001
 
 
@@ -41,7 +41,7 @@ class VehiclesStream(fleetioStream):
     name = "vehicles"
     path = "/v1/vehicles"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2024-01-01"
     schema_filepath = SCHEMAS_DIR / "vehicles.json"  # noqa: ERA001
 
 
@@ -50,7 +50,7 @@ class ExpenseEntriesStream(fleetioStream):
     name = "expense_entries"
     path = "/v1/expense_entries"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2023-03-01"
     schema_filepath = SCHEMAS_DIR / "expense_entries.json"  # noqa: ERA001
 
 
@@ -59,7 +59,7 @@ class ContactsStream(fleetioStream):
     name = "contacts"
     path = "/v2/contacts"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2023-03-01"
     schema_filepath = SCHEMAS_DIR / "contacts.json"  # noqa: ERA001
 
 
@@ -68,7 +68,7 @@ class FuelEntriesStream(fleetioStream):
     name = "fuel_entries"
     path = "/v1/fuel_entries"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2023-03-01"
     schema_filepath = SCHEMAS_DIR / "fuel_entries.json"  # noqa: ERA001
 
 
@@ -77,7 +77,7 @@ class PartsStream(fleetioStream):
     name = "parts"
     path = "/v1/parts"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2023-03-01"
     schema_filepath = SCHEMAS_DIR / "parts.json"  # noqa: ERA001
 
 
@@ -86,7 +86,7 @@ class PurchaseOrdersStream(fleetioStream):
     name = "purchase_orders"
     path = "/v1/purchase_orders"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2023-03-01"
     schema_filepath = SCHEMAS_DIR / "purchase_orders.json"  # noqa: ERA001
 
 
@@ -95,5 +95,5 @@ class VehicleAssignmentsStream(fleetioStream):
     name = "vehicle_assignments"
     path = "/v1/vehicle_assignments"
     primary_keys = ["id"]
-    replication_key = "updated_at"
+    api_version = "2024-01-01"
     schema_filepath = SCHEMAS_DIR / "vehicle_assignments.json"  # noqa: ERA001
